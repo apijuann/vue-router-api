a106f09b0dc256821009f78e8b9746fac6cf5711
Alfredo <apijuann@gmail.com>
2021-12-31 21:12:46 +0100

    router api

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Titulo texto="This is the about page..." />
   </div>
 </template>
+
+<script>
+import Titulo from '../components/Titulo.vue'
+export default {
+  components: {
+    Titulo
+  }
+}
+</script>
---
+++ src/views/Articulo.vue
@@ -0,0 +1,38 @@
+<template>
+  <Titulo texto="Ruta artículo con parámetros" />
+  <h3>Parametros: {{$route.params.id}}</h3>
+  <h3>{{articulo.title}}</h3>
+  <p>{{articulo.body}}</p>
+</template>
+
+<script>
+import Titulo from '../components/Titulo'
+export default {
+    components:{
+        Titulo
+    },
+    data() {
+        return {
+            articulo: {}
+        }
+    },
+    methods: {
+        async consumirArticulo(){
+            try {
+                const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+                const objeto = await data.json()
+                this.articulo = objeto
+            } catch (error) {
+            console.log(error)
+            }
+        }
+    },
+    created(){
+        this.consumirArticulo()
+    }
+}
+</script>
+
+<style>
+
+</style>
